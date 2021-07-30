6d56bfc626ff3ef4987a11b6565ebaf979191cf5
Pranto-275 <67626492+Pranto-275@users.noreply.github.com>
2021-07-30 20:36:13 +0600

    banner
    onlybanner update

---
+++ blog/resources/views/Home.blade.php
@@ -0,0 +1,17 @@
+@extends('Layout.app')
+
+@section('content')
+
+<div id="home">
+    <div class="image">
+        <div class="container">
+            <div class="row ">
+                <div class="col-12  d-flex align-items-center justify-content-center text-light darkknight p-2">
+                    <h1>The Dark Knight</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ blog/resources/views/Layout/footer.blade.php
@@ -0,0 +1,38 @@
+<section id="contact">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-12 col-sm-6 col-lg-5 py-5">
+                <h5 style="font-weight:bold">About Trainer</h5>
+                <p style="text-align: justify;">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+                    vel illum dolore eu feugiat nulla facilisis at vero Duis autem vel eum iriure dolor in
+                    hendrerit in vulputate velit esse eros et accumsan.pellentesque eget dolor gravida,
+                    tempus purus ac, ultricies mauris. Etiam est nisl,molestie sed egestas bibendum,variuseu diam</p>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 text-center py-5">
+                <h5  style="font-weight:bold">Social</h5>
+                <div class="d-flex flex-column">
+                    <div class="p-2">
+                        <h6>Facebook</h6>
+                    </div>
+                    <div class="p-2">
+                        <h6>Instagram</h6>
+                    </div>
+                    <div class="p-2">
+                        <h6>Twitter</h6>
+                    </div>
+                    <div class="p-2">
+                        <h6>What's App</h6>
+                    </div>
+
+
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-lg-4 py-5" style="text-align: justify;">
+                <h5  style="font-weight:bold">Contact Us</h5>
+                <p>Contact our 24/7 customer support if you have any questions. We'll help you out. <br>
+                copyright
+                <i class="fa fa-copyright" aria-hidden="true"></i> Atiqur pranto</p>
+            </div>
+        </div>
+    </div>
+</section>
